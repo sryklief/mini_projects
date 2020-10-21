607706a602c570c10d725a939e4d90e534e12865
sryklief <61458785+sryklief@users.noreply.github.com>
2020-10-21 14:13:14 +0200

    Add files via upload

---
+++ age_in_sec.py
@@ -0,0 +1,3 @@
+user_age = int(input('Enter your age: '))
+age_in_sec = user_age * 365 * 24 * 60 * 60
+print("You have lived fro {} seconds.".format(age_in_sec))
---
+++ chair_price.py
@@ -0,0 +1,8 @@
+price = int(input("price of chair: "))
+
+if price < 100:
+    print("buy the chair!!")
+elif price == 100:
+    print("You can if you want...")
+else:
+    print("Dont buy the chair!!")
