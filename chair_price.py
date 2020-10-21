0201fa2a7d54c34f9e42ea1bff126ca1522d1898
sryklief <61458785+sryklief@users.noreply.github.com>
2020-10-21 16:36:03 +0200

    Update chair_price.py

--- chair_price.py
+++ chair_price.py
@@ -4,8 +4,8 @@ from bs4 import BeautifulSoup
 user_budget = int(input("Enter your budget: "))
 
 content = requests.get("https://www.johnlewis.com/browse/home-garden/plants-planting/pots-planters/_/N-5urc").content
-soup = BeautifulSoup(content)
-element = soup.findAll("span", {"class": "product-card__price-span", "data-test": "product-card__price product-card__price--current"})
+soup = BeautifulSoup(content, "html.parser")
+element = soup.find("span", {"class": "product-card__price-span", "data-test": "product-card__price product-card__price--current"})
 price = element.text.strip()
 price_no_currency = price[1:]
 price_float = float(price_no_currency)
